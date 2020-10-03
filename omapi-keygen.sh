99522d87c857cbe2082848ec583ed411e35af3cd
CallMarl <PaHaGeek@gmail.com>
2020-10-03 18:52:17 +0200

    FIX: bug from omapi-keygen tool.

--- omapi-keygen.sh
+++ omapi-keygen.sh
@@ -1,21 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
 #Params
-public_key_file="key.pub"
-private_key_file="key"
+public_key_file="omapi_key.pub"
+private_key_file="omapi_key.pri"
 
 help()
 {
-    echo "Usage: $0"
-    echo "[algo] private_key magic_hash"
-    echo "algo:"
+    echo "Usage : $0 [algo] private_key magic_hash"
+    echo "available algo:"
     echo "  - hmac_md5"
-    echo "  - hmac_sha256"
 }
 
 warn1()
 {
     echo "Parametter are missing."
+    echo ""
     help
     exit 0;
 }
@@ -23,8 +22,8 @@ warn1()
 warn2()
 {
     echo "A key file already exist at this location."
-    echo "You're able to change public key file name by editing this script params,"
-    echo "or change your directory."
+    echo "Please remove them or change the location."
+    echo ""
     help
     exit 0
 }
@@ -43,27 +42,29 @@ check()
 
 hmac_md5()
 {
-    echo $1 > "./$private_key_file"
-    echo "$magic_hash" | openssl dgst -sha256 \
+    echo "private key : $private_key" > "./$private_key_file"
+    echo "magic hash : $magic_hash" >> "./$private_key_file"
+    echo "$magic_hash" | openssl dgst -md5 \
         -mac hmac \
-        -macopt key:$private_key \
-        -out $public_key_file
+        -macopt key:$private_key | cut -d " " -f 2 > $public_key_file
+    echo "creating ./key and ./key.pub file"
+    cat "$public_key_file"
 }
 
-private_key = $2
-magic_hash = $3
-check
+private_key=$2
+magic_hash=$3
 
 case "$1" in
     hmac_md5)
+        check
         hmac_md5
         ;;
     help)
         help
         ;;
-	*)
+        *)
         help
         exit 0
-		;;
+                ;;
 esac
 exit 0
